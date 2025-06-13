110b5d5fb306ac023f35c178b684dba8bbcf936d
JENkt4k <james.e.nelson@gmail.com>
2025-06-12 23:42:19 -0500

    unnecessary temp files cleanup

--- test-auth-oauth/src/passportStrategies.ts
+++
@@ -1,6 +0,0 @@
-import passport from 'passport';
-import { Strategy as LinkedInStrategy } from 'passport-linkedin-oauth2';
-import { Strategy as TwitterStrategy } from 'passport-twitter';
-import { User } from '../types/auth.types';
-
-// ...existing code for passport strategies...
